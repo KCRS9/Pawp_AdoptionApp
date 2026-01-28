4da2f505aff3ac930f41d70f12654104cd3cbd00
Kevin Rojas <kevinfisic@gmail.com>
2026-01-28 16:58:38 +0100

    refactor(model): Implementados cambio de la base de datos en los modelos de animal y usuarios

--- Backend/app/models/animals.py
+++ Backend/app/models/animals.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel
+from typing import Optional
 
-# -----------------------------------------------------------
 # Clase BASE: Campos comunes
-# -----------------------------------------------------------
 class AnimalBase(BaseModel):
     name: str
     species: str
@@ -11,28 +10,22 @@ class AnimalBase(BaseModel):
     size: str        # Ej: "Small", "Medium", "Large"
     description: str
     health: str      # Ej: "Vaccinated", "Neutered"
-    # status y shelter_id NO están aquí porque no son comunes a todos los inputs
+    profile_image: Optional[str] = None # NUEVO
 
-# -----------------------------------------------------------
 # Clase INPUT: Lo que recibimos al Crear/Editar
-# -----------------------------------------------------------
 class AnimalIn(AnimalBase):
     pass 
     # Hereda todo. No añadimos nada extra de momento.
     # El 'status' por defecto se manejará en BD ("Available").
 
-# -----------------------------------------------------------
 # Clase OUTPUT: Lo que devolvemos al frontend
-# -----------------------------------------------------------
 class AnimalOut(AnimalBase):
-    id: int
-    shelter_id: int
-    status: str      # Aquí sí mostramos el estado
+    id: str
+    shelter_id: str
+    status: str
 
-# -----------------------------------------------------------
 # Clase DB: Mapeo completo de la Tabla
-# -----------------------------------------------------------
 class AnimalDb(AnimalIn):
-    id: int
-    shelter_id: int
+    id: str
+    shelter_id: str
     status: str
