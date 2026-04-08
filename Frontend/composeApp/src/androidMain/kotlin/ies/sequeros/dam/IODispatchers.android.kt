92683649a9f347e56fe9d7ca3fc718f045d6678a
Kevin Rojas <kevinfisic@gmail.com>
2026-04-08 22:54:39 +0200

    feat(auth): añadido IODispatchers para cada plataforma

---
+++ Frontend/composeApp/src/androidMain/kotlin/ies/sequeros/dam/IODispatchers.android.kt
@@ -0,0 +1,9 @@
+package ies.sequeros.dam
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+actual object IODispatchers {
+
+    actual val io: CoroutineDispatcher = Dispatchers.IO
+}
