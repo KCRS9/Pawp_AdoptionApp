ad40ccf4abcbe45e101d51f52fcf901c54e1c492
Kevin Rojas <kevinfisic@gmail.com>
2026-04-10 01:02:46 +0200

    feat(auth): inicializacion de Koin en cada plataforma

--- Frontend/composeApp/src/androidMain/kotlin/ies/sequeros/dam/MainActivity.kt
+++ Frontend/composeApp/src/androidMain/kotlin/ies/sequeros/dam/MainActivity.kt
@@ -6,6 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.russhwolf.settings.SharedPreferencesSettings
+import ies.sequeros.dam.di.appModule
+import org.koin.compose.KoinApplication
+import org.koin.core.KoinApplication
+import org.koin.dsl.module
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +18,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            KoinApplication(application = {
+                modules(
+                    appModule,
+                    module {
+                        single {
+                            SharedPreferencesSettings(
+                                getSharedPreferences("pawp_prefs", MODE_PRIVATE)
+                            )
+                        }
+                    }
+
+                )
+            }){
+                App()
+            }
         }
     }
 }
--- Frontend/composeApp/src/jvmMain/kotlin/ies/sequeros/dam/main.kt
+++ Frontend/composeApp/src/jvmMain/kotlin/ies/sequeros/dam/main.kt
@@ -2,12 +2,30 @@ package ies.sequeros.dam
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.russhwolf.settings.PreferencesSettings
+import ies.sequeros.dam.di.appModule
+import org.koin.compose.KoinApplication
+import org.koin.dsl.module
+import java.util.prefs.Preferences
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "pawp_adoption",
     ) {
-        App()
+        KoinApplication(application = {
+
+            modules(
+                appModule,
+                module{
+                    single{
+                        PreferencesSettings(Preferences.userRoot().node("pawp"))
+                    }
+                }
+            )
+        }){
+            App()
+        }
+
     }
 }
