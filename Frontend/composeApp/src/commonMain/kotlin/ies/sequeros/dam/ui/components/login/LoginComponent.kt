e9c3ada0bde693abcba42e39ca09f19978e83cf9
Kevin Rojas <kevinfisic@gmail.com>
2026-04-10 00:22:33 +0200

    refactor(LoginComponet): Mejorada la organizacion de los componentes

--- Frontend/composeApp/src/commonMain/kotlin/ies/sequeros/dam/ui/components/login/LoginComponent.kt
+++ Frontend/composeApp/src/commonMain/kotlin/ies/sequeros/dam/ui/components/login/LoginComponent.kt
@@ -1,4 +1,4 @@
-package ies.sequeros.dam.ui.components
+package ies.sequeros.dam.ui.components.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -26,7 +25,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ies.sequeros.dam.ui.login.LoginState
 import org.jetbrains.compose.resources.painterResource
--- Frontend/composeApp/src/commonMain/kotlin/ies/sequeros/dam/ui/login/LoginScreen.kt
+++ Frontend/composeApp/src/commonMain/kotlin/ies/sequeros/dam/ui/login/LoginScreen.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import ies.sequeros.dam.ui.components.LoginComponent
+import ies.sequeros.dam.ui.components.login.LoginComponent
 import org.koin.compose.viewmodel.koinViewModel
 
 
