57c0692dc350c6627f6d71a24a6dd24e75c1e565
Kevin Rojas <kevinfisic@gmail.com>
2026-04-10 02:56:45 +0200

    feat(theme): añadidas constantes de tamaño de tipografía

---
+++ Frontend/composeApp/src/commonMain/kotlin/ies/sequeros/dam/ui/theme/Type.kt
@@ -0,0 +1,12 @@
+package ies.sequeros.dam.ui.theme
+
+import androidx.compose.ui.unit.sp
+
+/** Tamaño de los títulos principales (Poppins SemiBold) */
+val TitleFontSize = 32.sp
+
+/** Tamaño del texto de cuerpo (Poppins Regular) */
+val BodyFontSize = 16.sp
+
+/** Tamaño de texto secundario / labels */
+val SmallFontSize = 14.sp
