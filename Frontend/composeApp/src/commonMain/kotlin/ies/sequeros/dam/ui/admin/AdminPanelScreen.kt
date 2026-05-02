23a911e0e192942ab83eebb14f4381d5f0f7415f
Kevin Rojas <kevinfisic@gmail.com>
2026-05-02 19:56:27 +0200

    fix(aminPanelScreen): Agrgeado scroll para poder visualizar todo el contenido de la pantalla

--- Frontend/composeApp/src/commonMain/kotlin/ies/sequeros/dam/ui/admin/AdminPanelScreen.kt
+++ Frontend/composeApp/src/commonMain/kotlin/ies/sequeros/dam/ui/admin/AdminPanelScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -75,7 +77,8 @@ fun AdminPanelScreen(
             Column(
                 modifier = Modifier
                     .widthIn(max = 480.dp)
-                    .fillMaxSize()
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
                     .padding(horizontal = 16.dp, vertical = 8.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
