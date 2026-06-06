987ac9ea6d9e99659daa7eb09b047a01e92bec00
Kevin Rojas <kevinfisic@gmail.com>
2026-06-06 17:47:31 +0200

    Corrige la URL de origen permitida en la configuración de CORS

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8081",
         "http://localhost:8080",
-        "https://pawpadoptionapp-production-993b.up.railway.app"
+        "https://pawpapp.up.railway.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
